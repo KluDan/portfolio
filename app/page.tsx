3738b5b26f3dad116c79abe1af5b02db3725a6e9
Danylo Kliushnychenko <danylktv2@gmail.com>
2024-05-21 16:48:25 +0100

    feat: add About section with grid and animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import AboutGrid from "@/components/AboutGrid";
 import Hero from "@/components/Hero";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
+        <AboutGrid />
       </div>
     </main>
   );
